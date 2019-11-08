da45f13338d98104bc2497f3aebe9246476512fd
陈三郎 <chensanlang@ywwl.com>
2019-11-08 16:35:50 +0800

    feat ejs哦使

--- blog-server/src/routes/index.js
+++ blog-server/src/routes/index.js
@@ -1,18 +1,45 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  // 异步读取模板文件
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    message: '你好',
+    isMe: true,
+    blogList: [
+      {
+        title: 'aaa'
+      }, {
+        title: 'bbb'
+      }, {
+        title: 'ccc'
+      }, {
+        title: 'ddd'
+      }
+    ]
   })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const { username } = ctx.params
+  ctx.body = {
+    title: '登陆者姓名',
+    username
+  }
+})
+
+router.get('/loadmore/:username/:pagenum', async (ctx, next) => {
+  const { username, pagenum } = ctx.params
+  ctx.body = {
+    title: '查询页数',
+    username,
+    pagenum
   }
 })
 
--- blog-server/src/routes/users.js
+++ blog-server/src/routes/users.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 
-router.prefix('/users') // 前缀
+router.prefix('/users') // 路由前缀
 
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
@@ -8,6 +8,14 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+router.post('/login', async (ctx, next) => {
+  const { username, password } = ctx.request.body
+  ctx.body = {
+    username,
+    password
+  }
 })
 
 module.exports = router
